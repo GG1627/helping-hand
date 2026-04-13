abc54a6e941e859c52d37eb138db635372018475
ssrik-1 <shrinitha@gmail.com>
2026-04-13 00:19:38 -0400

    firebase connection

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/main_shell.dart';
 import 'screens/start_screen.dart';
 import 'theme/warm_clay_theme.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  /* try {
+  await FirebaseFirestore.instance
+      .collection('connection_test')
+      .add({'timestamp': DateTime.now().toIso8601String()});
+  print('Firebase connection verified!');
+} catch (e) {
+  print('Firebase connection failed: $e');
+} */
+
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
   runApp(const HelpingHandApp());
 }
 
