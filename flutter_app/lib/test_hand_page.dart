be3bf2d952c090be2cf0f389a50b4357c7446b77
Brian Paz <bpaz@ufl.edu>
2026-09-10 03:50:04 -0400

    Add selectable 3D hand poses for A, B, C

--- flutter_app/lib/test_hand_page.dart
+++ flutter_app/lib/test_hand_page.dart
@@ -10,10 +10,11 @@ class TestHandPage extends StatefulWidget {
 
 class _TestHandPageState extends State<TestHandPage> {
   final Flutter3DController _controller = Flutter3DController();
+  
+  final List<String> letters = ['A', 'B', 'C'];
   String selectedLetter = 'A';
 
   void _applyPose(String letter) {
-    // If your GLB model contains baked skeletal animations named 'Pose_A', 'Pose_B', etc.:
     _controller.playAnimation(animationName: 'Pose_$letter');
   }
 
@@ -26,8 +27,8 @@ class _TestHandPageState extends State<TestHandPage> {
           DropdownButton<String>(
             value: selectedLetter,
             dropdownColor: Colors.grey[800],
-            style: const TextStyle(color: Colors.white, fontSize: 18),
-            items: ASLPoseDictionary.poses.keys.map((String letter) {
+            style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 18),
+            items: letters.map((String letter) {
               return DropdownMenuItem<String>(
                 value: letter,
                 child: Text('Letter $letter'),
@@ -49,9 +50,13 @@ class _TestHandPageState extends State<TestHandPage> {
           height: MediaQuery.of(context).size.height * 0.7,
           child: Flutter3DViewer(
             controller: _controller,
-            src: 'assets/models/test.glb',
-            onLoad: (String modelName) {
+            src: '/assets/models/test.glb',
+            onLoad: (String modelName) async {
               debugPrint('Model $modelName loaded!');
+              
+              List<String> tracks = await _controller.getAvailableAnimations();
+              debugPrint('Available NLA tracks in GLB: $tracks');
+
               _applyPose(selectedLetter);
             },
           ),
